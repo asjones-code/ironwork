eafac024ad8a89ac07323ac54161feb6f1d2695f
asjones-code <asjones91@gmail.com>
2020-09-11 10:06:32 -0400

    brrr

--- Crunchbase.py
+++ Crunchbase.py
@@ -72,6 +72,11 @@ while x<len(companyinfo):
         print(companydf.loc[x])
         
         time.sleep(scrapenice)
+        diviz = x/5
+          if diviz.is_integer():
+              print ('pause for 2 min')
+              time.sleep(120)
+        
         
 
         x+=1
