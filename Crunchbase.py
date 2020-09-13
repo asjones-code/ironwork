689a9e891a5ce8c727306759af8c6fcf694e0432
asjones-code <asjones91@gmail.com>
2020-09-12 23:21:49 -0400

    brrr

--- Crunchbase.py
+++ Crunchbase.py
@@ -4,6 +4,7 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 #from selenium.webdriver.firefox.options import Options
 #from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
@@ -14,7 +15,14 @@ from sqlalchemy import create_engine
 
 waitarray = [3,5,7,9, 30]
 scrapenice = np.random.choice (waitarray)
+myProxy = FreeProxy(country_id=['US']).get()
 
+proxy = Proxy({
+    'proxyType': ProxyType.MANUAL,
+    'httpProxy': myProxy,
+    'ftpProxy': myProxy,
+    'sslProxy': myProxy,
+    })
 
 #SQLALCHEMY
 engine = create_engine("postgresql+psycopg2://dtqkynygrntpco:f8b2d26aee326c186e71fcc28ffad460d698d06e4456c41b75ffa4b315750938@ec2-54-172-173-58.compute-1.amazonaws.com:5432/d3dk2h0pspg85c")
@@ -25,7 +33,7 @@ engine = create_engine("postgresql+psycopg2://dtqkynygrntpco:f8b2d26aee326c186e7
 options = FirefoxOptions()
 options.add_argument('--no-sandbox')
 options.add_argument("--headless")
-driver = webdriver.Firefox(options=options, executable_path=os.environ.get("GECKODRIVER_PATH"),firefox_binary=os.environ.get("FIREFOX_BIN"))
+driver = webdriver.Firefox(options=options, proxy=proxy, executable_path=os.environ.get("GECKODRIVER_PATH"),firefox_binary=os.environ.get("FIREFOX_BIN"))
 
 
 
@@ -76,7 +84,10 @@ while x<len(companyinfo):
         diviz = x/4
         if diviz.is_integer():
             print ('pause for 2 min')
+            myProxy = FreeProxy(country_id=['US'], rand= True).get()
+            print(myProxy)
             time.sleep(120)
+            
         
         
 
